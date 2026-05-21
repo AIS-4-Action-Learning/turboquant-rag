370b2d446c1a29f0570c8fb880c5577e134cc26e
HamzaElhmd <H.Elhamdi@aui.ma>
2026-05-21 09:16:45 +0200

    LlamaBF16 patch: Loading and Initialization

--- app/llama_models.py
+++ app/llama_models.py
@@ -419,7 +419,7 @@ class LlamaGenerator:
                 for i in range(max_gen_len):
                     if i != 0:
                         end_time_t = time.perf_counter()
-                        print(f"[INFO] Token {i - 1} took {start_time_t - end_time_t:.4f}")
+                        print(f"[INFO] Token {i - 1} took {end_time_t - start_time_t:.4f}")
 
                     print(f"[INFO] Token {i} forward...")
                     start_time_t = time.perf_counter()
